da32c6692b741bbd3c71e765263d5b3d68bd8021
illiabranchuk <jointlord0@gmail.com>
2024-04-29 16:04:07 +0100

    data integrity fix

--- trani_model.py
+++ trani_model.py
@@ -1,15 +1,24 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
-train = np.genfromtxt('train.csv', delimiter=',')
-result = np.genfromtxt('train.csv', delimiter=',')
 
-# Data to learn on it
-X_train = train[1:, 1:]
-y_train = result[1:, 1:]
+X_train =  pd.read_csv('train.csv')
+y_train = X_train.pop('Survived')
+
+label_encoder = LabelEncoder()
+
+X_train['Name'] = X_train['Name'].apply(lambda x:  label_encoder.fit_transform(x))
+X_train['Sex'] = X_train['Sex'].apply(lambda x:  label_encoder.fit_transform(x))
+X_train['Ticket'] = X_train['Ticket'].apply(lambda x:  label_encoder.fit_transform(x))
+X_train['Embarked'] = X_train['Embarked'].apply(lambda x:  label_encoder.fit_transform(x))
+
+
+print(y_train)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(128, activation='relu', input_shape=(11,)),
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(13,)),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
