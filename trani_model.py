a1564383c195197b0d1fbade7a2dada314323288
illiabranchuk <jointlord0@gmail.com>
2024-05-03 22:26:43 +0100

    Added predictions

--- trani_model.py
+++ trani_model.py
@@ -4,21 +4,34 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
+def prepareData(data):
+    data.pop('Ticket')
+    if 'Survived' in data:
+        data.pop('Survived')
+    data.pop('Cabin')
+
+    mean_age = data['Age'].mean()
+
+    label_encoder = LabelEncoder()
+    data['Name'] = label_encoder.fit_transform(data['Name'].astype(str))
+    data['Sex'] = label_encoder.fit_transform(data['Sex'])
+    data['Embarked'] = label_encoder.fit_transform(data['Embarked'])
+
+    data['Age'] = data['Age'].apply(lambda x: mean_age if pd.isnull(x) else x)
+
+    data['Embarked'] = label_encoder.fit_transform(data['Embarked'].astype(str))
+    return data 
+
+
+def binary_prediction(probability):
+    if probability >= 0.5:
+        return 1
+    else:
+        return 0
+    
 X_train =  pd.read_csv('train.csv')
 y_train = X_train.pop('Survived')
-X_train.pop('Ticket')
-X_train.pop('Cabin')
-
-mean_age = X_train['Age'].mean()
-
-label_encoder = LabelEncoder()
-X_train['Name'] = label_encoder.fit_transform(X_train['Name'].astype(str))
-X_train['Sex'] = label_encoder.fit_transform(X_train['Sex'])
-X_train['Embarked'] = label_encoder.fit_transform(X_train['Embarked'])
-
-X_train['Age'] = X_train['Age'].apply(lambda x: mean_age if pd.isnull(x) else x)
-
-X_train['Embarked'] = label_encoder.fit_transform(X_train['Embarked'].astype(str))
+X_train = prepareData(X_train)
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(9,)),
@@ -34,4 +47,14 @@ model.compile(optimizer='adam',
 # Train the model
 model.fit(X_train, y_train, epochs=10, batch_size=32)
 
+# Make predictions
+test_data =  pd.read_csv('test.csv')
+test_data = prepareData(test_data)
+predictions = model.predict(test_data)
+
+for i in range(len(predictions)):
+    predictions[i] = binary_prediction(predictions[i]) 
+
+# Print predictions
+print(predictions)
 
